1510bf4efdf2dda9f184634de3d6a6361f715631
wangyufu19 <youfu.wang@gmail.com>
2019-06-11 10:59:57 +0800

    commit

--- jboscloud-openapi/src/main/java/com/jboscloud/JbosCloudOpenApiApplication.java
+++ jboscloud-openapi/src/main/java/com/jboscloud/JbosCloudOpenApiApplication.java
@@ -1,7 +1,10 @@
 package com.jboscloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import com.jboscloud.openapi.filter.GatewayFilter;
 
@@ -12,6 +15,12 @@ public class JbosCloudOpenApiApplication {
 	@Bean
 	public GatewayFilter gatewayFilter(){
 		return new GatewayFilter();
+	}
+	@Bean
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties(){
+		return new ZuulProperties();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JbosCloudOpenApiApplication.class, args);
