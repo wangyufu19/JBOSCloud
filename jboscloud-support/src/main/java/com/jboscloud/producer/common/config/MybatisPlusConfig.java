522bcea01872cc5922ef0e1b082f5b3f92e8f9da
youfu.wang <youfu.wang@gmail.com>
2021-04-22 13:46:17 +0800

    commit

--- jboscloud-support/src/main/java/com/jboscloud/producer/common/config/MybatisPlusConfig.java
+++ jboscloud-support/src/main/java/com/jboscloud/producer/common/config/MybatisPlusConfig.java
@@ -1,10 +1,31 @@
 package com.jboscloud.producer.common.config;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 @Configuration
 @MapperScan(basePackages = "com.jboscloud.producer.mapper")
 public class MybatisPlusConfig {
-
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+    @Bean
+    PageHelper pageHelper(){
+        //分页插件
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "oracle");
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        properties.setProperty("returnPageInfo", "check");
+        properties.setProperty("params", "count=countSql");
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
 
 }
