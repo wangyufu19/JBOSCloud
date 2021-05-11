663d329f8ebd299bfbd3d4c44e0c5794acf8ed0c
youfu.wang <youfu.wang@gmail.com>
2021-05-11 17:24:56 +0800

    commit

--- jboscloud-config/src/main/java/com/jboscloud/JboscloudConfigApplication.java
+++ jboscloud-config/src/main/java/com/jboscloud/JboscloudConfigApplication.java
@@ -3,11 +3,13 @@ package com.jboscloud;
 import com.jboscloud.kafka.KafkaSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class JboscloudConfigApplication {
 
 	public static void main(String[] args) {
